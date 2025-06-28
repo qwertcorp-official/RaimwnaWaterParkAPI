455a70d63e7abc4177991fed92aa334ae29e55f9
lingdao-dwi <lingdaodaimary@gmail.com>
2025-06-28 16:06:43 +0530

    tests

--- api_v1/index.php
+++ api_v1/index.php
@@ -6,10 +6,10 @@ ini_set('log_errors', 1);
 ini_set('error_log', 'php_errors.log');
 
 // Set headers first
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+// header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 error_log("🔥 Debug Start: index.php running");
 
--- api_v1/transactionlist.php
+++ api_v1/transactionlist.php
@@ -1,8 +1,5 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Content-Type: application/json'); 
 echo json_encode(
     [
         "status" => "success",
